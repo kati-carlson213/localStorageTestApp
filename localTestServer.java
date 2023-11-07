6ea168719da60c1af688b69857a6b2aa7ff0ba1e
Katie <112095351+kati-carlson213@users.noreply.github.com>
2023-11-06 21:07:38 -0500

    Update localTestServer.java

--- localTestServer.java
+++ localTestServer.java
@@ -1,13 +1,15 @@
 import java.io.*;
 import java.net.*;
-import java.nio.file.*;
+import java.util.*;
 
 class Server {
   private ServerSocket server = null;
+  private String storagePath = "received_files"; // Directory for storing received files
 
   public Server(int port, int backlog, String IP) {
     try {
       server = new ServerSocket(port, backlog, InetAddress.getByName(IP));
+      System.out.println("Server started.");
 
       while (true) {
         Socket socket = server.accept();
@@ -19,66 +21,62 @@ class Server {
   }
 
   public static void main(String args[]) {
-    Server server = new Server(5000, 0, "127.0.0.1"); //change IP on both server + client side to machine's IP when testing to see how it works with public IP's  
-  }
-}
-
-class ClientHandler extends Thread {
-  private Socket socket;
-
-  public ClientHandler(Socket socket) {
-    this.socket = socket;
+    Server server = new Server(5000, 0, "127.0.0.1");
   }
 
-  @Override
-  public void run() {
-    try {
-      // Create a directory to store received files
-      File storageDirectory = new File("received_files");
-      storageDirectory.mkdir();
+  class ClientHandler extends Thread {
+    private Socket socket;
 
-      InputStream inputStream = socket.getInputStream();
-      byte[] buffer = new byte[1024];
-      int bytesRead;
+    public ClientHandler(Socket socket) {
+      this.socket = socket;
+    }
 
-      while (true) {
-        int headerLength = inputStream.read(buffer, 0, buffer.length);
-        if (headerLength <= 0) {
-          break; // No more data to read
-        }
+    @Override
+    public void run() {
+      try {
+        InputStream inputStream = socket.getInputStream();
+        byte[] buffer = new byte[1024];
+        int bytesRead;
 
-        String header = new String(buffer, 0, headerLength);
-        int startIndex = header.indexOf("filename:");
-        if (startIndex != -1) {
-          int endIndex = header.indexOf("\r\n", startIndex);
-          if (endIndex != -1) {
-            String fileName = header.substring(startIndex + 9, endIndex);
+        while (true) {
+          int headerLength = inputStream.read(buffer, 0, buffer.length);
+          if (headerLength <= 0) {
+            break; // No more data to read
+          }
 
-            File receivedFile = new File(storageDirectory, fileName);
+          String header = new String(buffer, 0, headerLength);
 
-            try (FileOutputStream fileOutputStream = new FileOutputStream(receivedFile)) {
-              while (true) {
-                bytesRead = inputStream.read(buffer, 0, buffer.length);
-                if (bytesRead <= 0) {
-                  break; // No more data to read
-                }
-                if (new String(buffer, 0, bytesRead).contains("END_OF_FILE")) {
-                  break; // End of current file
-                }
-                fileOutputStream.write(buffer, 0, bytesRead);
-              }
-
-            } catch (Exception e) {
-              System.out.println("Error saving file: " + e.getMessage());
-            }
+          if (header.startsWith("retrieve:")) {
+            // Handle file retrieval request
+            String fileName = header.substring(9, header.length() - 2); // Extract file name
+            receiveFile(fileName, inputStream);
           }
         }
+      } catch (Exception e) {
+        System.out.println("Error: " + e.getMessage());
+      } finally {
+        try {
+          socket.close();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
       }
-    } catch (Exception e) {
-      System.out.println("Error receiving file: " + e.getMessage());
-    } finally {
+    }
+
+    private void receiveFile(String fileName, InputStream inputStream) {
       try {
-        socket.close();
+        // Create a FileOutputStream to write the received file
+        File outputFile = new File(storagePath, fileName);
+        FileOutputStream fileOutputStream = new FileOutputStream(outputFile);
+
+        byte[] buffer = new byte[1024];
+        int bytesRead;
+
+        while ((bytesRead = inputStream.read(buffer)) != -1) {
+          fileOutputStream.write(buffer, 0, bytesRead);
+        }
+
+        fileOutputStream.close();
       } catch (IOException e) {
         e.printStackTrace();
       }
